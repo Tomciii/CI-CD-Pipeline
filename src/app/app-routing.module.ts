cd3426ba9c78271f7273e6f9162467e8fe7c4952
tomcii <116713@fhwn.ac.at>
2023-10-15 22:49:36 +0200

    Add basic Components and Pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./pages/home/home.component";
+import {GalleryComponent} from "./pages/gallery/gallery.component";
+import {AboutComponent} from "./pages/about/about.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'gallery', component: GalleryComponent},
+  {path:'about', component: AboutComponent},
+  {path: '**', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { AboutComponent } from './pages/about/about.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GalleryComponent,
+    AboutComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
